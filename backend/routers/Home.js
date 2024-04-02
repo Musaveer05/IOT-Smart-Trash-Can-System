90c365fad1e05f05455c0c6100e859c346f207fb
Musaveer05 <93473939+Musaveer05@users.noreply.github.com>
2024-04-02 21:51:09 +0530

    Update MQTT Subscription

--- backend/routers/Home.js
+++ backend/routers/Home.js
@@ -3,31 +3,44 @@ const jwtAuthenticate = require('../middleware/jwtAuthenticate')
 const router = express.Router()
 
 const mqtt = require('mqtt')
-const WebSocket = require('ws')
-
 const mqttConnection = {
     clientId: process.env.CLIENT_ID_MQTT,
     username: process.env.USERNAME_MQTT,
-    password: process.env.PASSWORD_MQTT
+    password: process.env.PASSWORD_MQTT,
+    clean: true,
+    connectTimeout: 4000
 }
 
-const mqttClient = mqtt.connect('mqtt://mqtt.thingspeak.com', mqttConnection)
-const mqttTopic = 'trash-bin-data'
-const wss = new WebSocket.Server({port:8080})
 
-wss.on('connection', (ws) =>{
-    console.log('websocket client connected')
+const mqttClient = mqtt.connect('mqtt://mqtt3.thingspeak.com', mqttConnection)
+const mqttTopic = "channels/2480503/subscribe"
 
-    mqttClient.subscribe(mqttTopic)
 
-    
+mqttClient.on('error', (error) => {
+    console.error('MQTT client error:', error);
+})
 
+
+mqttClient.on('connect', () => {
+    console.log('Connected to MQTT broker');
+    mqttClient.subscribe(mqttTopic, (err) => {
+        if (err) {
+            console.error('Error subscribing to MQTT topic:', err);
+        } else {
+
+            console.log('Subscribed to MQTT topic:', mqttTopic);
+
+        }
+    });
+});
+
+
+mqttClient.on('message', (mqttTopic, payload) => {
+    console.log('Received Message:', mqttTopic, payload.toString())
 })
 
 router.get('/', jwtAuthenticate, (req,res) =>{
     res.send(req.rootUser)
-
-
 })
 
 module.exports = router;
